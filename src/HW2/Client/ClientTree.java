ddc81120cf8b3ffbd52a6d8d9cb55debbeff9cd3
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-03-10 18:34:03 +0500

    Add old HW

---
+++ src/HW2/Client/ClientTree.java
@@ -0,0 +1,7 @@
+package Client;
+
+public class ClientTree {
+    public static void main(String[] args) {
+        ClientAdapter clientAdapter = new ClientAdapter("127.0.0.1", 8888);
+    }
+}
---
+++ src/HW2/Server/ServerStart.java
@@ -0,0 +1,14 @@
+package Server;
+
+ class ServerStart {
+    public static void main(String[] args) {
+        Server server = new Server(8888);
+    }
+    enum B {
+
+    }
+
+    enum A{
+
+    }
+}
