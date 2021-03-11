bb464d6d7b31317b7e92294a77948a79a31775c1
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-03-11 18:35:25 +0500

    connect to DB

--- src/HW2/Server/DB/ConnectToDB.java
+++ src/HW2/Server/DB/ConnectToDB.java
@@ -1,17 +1,24 @@
 package HW2.Server.DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ConnectToDB {
+    private static final String url = "jdbc:mysql://localhost:3306/Java3";
+    private static final String user = "root";
+    private static final String pass = "root";
     private static Connection con;
     private static Statement stmt;
     private static ResultSet rs;
 
     public static void main(String[] args) {
-        con= DriverManager.getConnection(jdbc:mysql://localhost:3306/test)
+        try {
+            con= DriverManager.getConnection(url, user, pass);
+            stmt=con.createStatement();
+            stmt.executeUpdate("INSERT INTO LoginBase (Login, Password, Nickname) VALUES ('L1', 'P1', 'N1')");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 }
