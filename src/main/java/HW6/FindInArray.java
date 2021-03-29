e71132a6602d73995e6ebc31dc2d89605cc8ec82
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-03-29 21:22:56 +0500

    Complete

--- src/main/java/HW6/FindInArray.java
+++ src/main/java/HW6/FindInArray.java
@@ -1,9 +1,18 @@
 package HW6;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class FindInArray {
-    public int[] returnAllAfterLast4 (int[] array){
+    public static void main(String[] args) {
+        FindInArray findInArray = new FindInArray();
+        System.out.println(Arrays.toString(
+                findInArray.returnAllAfterLastFor(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7})));
+        System.out.println(
+                findInArray.isArrayHaveOneOrFor(new int[]{4, 4, 4}));
+    }
+
+    public int[] returnAllAfterLastFor (int[] array){
         boolean is4Founded = false;
         Stack<Integer> returnedStack = new Stack<>();
         for (int i= array.length-1; i>=0; i--){
@@ -15,17 +24,22 @@ public class FindInArray {
         }
         if (is4Founded) {
             int[] returnedArray = new int[returnedStack.size()];
-            for (int i=0; i<returnedStack.size(); i++){
+            int size = returnedStack.size();
+            for (int i=0; i<size; i++){
                 returnedArray[i] = returnedStack.pop();
             }
             return returnedArray;
         } else  throw new RuntimeException("Array have not 4.");
     }
-    public boolean isArrayHave1Or4 (int[] array){
+    public boolean isArrayHaveOneOrFor (int[] array){
+        boolean isArrayHave1 = false;
+        boolean isArrayHave4 = false;
         for (int nod: array) {
-            if (nod == 1 || nod == 4) return true;
+            if (nod != 1 && nod != 4) return false;
+            if (nod == 1) isArrayHave1 = true;
+            if (nod == 4) isArrayHave4 = true;
         }
-        return false;
+        return isArrayHave1 && isArrayHave4;
     }
 }
 
